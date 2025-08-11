d3e8cc79cc440d5747c2bf125b15c09926b2d6b1
kitajusSusWWA <kitaj2458@gmail.com>
2025-08-11 23:02:41 +0200

    Przeprowadzanie wielu symulacji na raz i wyświetlanie ANALIZY PCA

--- src/test_1.jl
+++ src/test_1.jl
@@ -1,82 +1,33 @@
+using Revise
+
 include("lib.jl")
+include("pca.jl")
+
 using .modHydroSim
+using .PCAWorkflow
 using Plots
-gr()
 
-
-export run_and_plot, plot_trajectories, create_points_animation
-"""
-    plot_trajectories(wynik_symulacji::SimResult)
-
-Rysuje statyczne trajektorie na podstawie gotowych wyników symulacji.
-"""
-function plot_trajectories(wynik_symulacji::SimResult)
-    println("Generowanie wykresu trajektorii...")
-    p = plot(title="Trajektorie w przestrzeni fazowej", xlabel="Temperatura T [MeV]", ylabel="Anizotropia A", legend=false)
-    for sol in wynik_symulacji.solutions
-        plot!(p, sol, idxs=(1, 2))
-    end
-    return p
-end
-
-"""
-    create_points_animation(wynik_symulacji::SimResult; ...)
-
-Tworzy i zapisuje animację GIF pokazującą ewolucję punktów w czasie.
-"""
-function create_points_animation(
-    wynik_symulacji::SimResult;
-    output_filename="ewolucja_punktow.gif",
-    fps=2,
-    frames=200
-)
-    println("Rozpoczynam generowanie animacji punktów...")
-
-    t_start, t_end = wynik_symulacji.settings.tspan
-    tau_values = range(t_start, stop=t_end, length=frames)
-
-    # @animate
-    anim = @animate for (idx, tau) in enumerate(tau_values)
-        print("\rGenerowanie klatki $(idx)/$(frames)...") 
-
-        # Stwórz bazowy wykres dla pojedynczej klatki
-        p = plot(
-            title="Ewolucja punktów (τ = $(round(tau, digits=3)))",
-            xlabel="Temperatura T [MeV]",
-            ylabel="Anizotropia A",
-            # Stałe granice osi są kluczowe dla dobrej animacji!
-            xlims=(0.0, wynik_symulacji.settings.T_range[2]),
-            ylims=(-1.0, wynik_symulacji.settings.A_range[2] + 1)
-        )
-
-        # Dla każdego rozwiązania,
-        for (i, sol)in enumerate(wynik_symulacji.solutions)
-            aktualny_stan = sol(tau)
-            scatter!(p, [aktualny_stan[1]], [aktualny_stan[2]], label="", seriescolor=i, markersize=3)
-        end
-    end
-
-    # Zapisz animację do pliku GIF
-    gif(anim, output_filename, fps=fps)
-    println("\nAnimacja została zapisana jako: $(output_filename)")
-end
-
-
-"""
-    run_and_plot(settings::SimSettings = SimSettings())
-
-Główna funkcja orkiestrująca: uruchamia symulację i rysuje statyczny wykres.
-"""
-function run_and_plot(settings::SimSettings = SimSettings())
-    println("--- Rozpoczynam symulację... ---")
-    wynik = run_simulation(settings)
-    println("--- Symulacja zakończona. ---")
+function run_all_pca_and_plot()
+    # Definicje zakresów temperatur (w MeV)
+    temp_ranges = [
+        (300.0, 600.0),
+        (400.0, 700.0),
+        (500.0, 800.0),
+        (600.0, 1200.0),
+    ]
     
-    wykres = plot_trajectories(wynik)
-    display(wykres)
-    println("\nWykres statyczny gotowy.")
+    plots = []
     
-    return (wynik, wykres)
+    for (i, (Tmin, Tmax)) in enumerate(temp_ranges)
+        settings = modHydroSim.SimSettings(T_range = (Tmin * modHydroSim.MeV, Tmax * modHydroSim.MeV))
+        sim = modHydroSim.run_simulation(settings = settings)
+        pca_results = PCAWorkflow.calc_pca(sim, features_indices = (1, 2), n_steps = 1000, n_components = 2)
+        
+        plt = PCAWorkflow.p_explained_variance(pca_results; prefix = "T=$(Tmin)-$(Tmax)")
+        push!(plots, plt)
+    end
+    
+    plot(plots..., layout = (2, 2), size=(1000,800), titlefontsize=10, guidefontsize=9, legendfontsize=8)
 end
 
-println("Plik `test_1.jl` wczytany. Dostępne funkcje: `run_and_plot`, `plot_trajectories`, `create_points_animation`.")
+run_all_pca_and_plot()
